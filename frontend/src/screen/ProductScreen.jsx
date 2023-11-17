b554689ddaf9d314a3fc9a7a8726b99a53223200
phyothantzin <phyothantzin.w@gmail.com>
2023-11-17 17:34:02 +0630

    added express backend

--- frontend/src/screen/ProductScreen.jsx
+++ frontend/src/screen/ProductScreen.jsx
@@ -1,12 +1,20 @@
 import { Link, useParams } from "react-router-dom";
-import products from "../products";
 import Rating from "../components/Rating";
 import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
 
-  const product = products.find((product) => product._id === productId);
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
