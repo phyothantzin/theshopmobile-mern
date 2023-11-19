d0bc5e5b47d54874080186f5f639bc4ef345b700
phyothantzin <phyothantzin.w@gmail.com>
2023-11-19 19:02:18 +0630

    implement authentication backend

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,23 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
 const port = process.env.PORT || 8000;
 
 connectDB(); // Connect to MongoDB
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req, res) => res.send("API is running..."));
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
